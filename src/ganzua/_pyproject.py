71a0a709628242fe542a0fcc04c44fe332f10098
Lukas Atkinson <opensource@LukasAtkinson.de>
2025-08-31 10:34:14 +0200

    change TOML traversal to a more DOM-like approach
    There are now `TomlRef` objects that can be replaced directly, without having to
    track parent keys.

--- src/ganzua/_pyproject.py
+++ src/ganzua/_pyproject.py
@@ -5,23 +5,18 @@ import tomlkit
 import tomlkit.container
 import tomlkit.items
 from packaging.requirements import Requirement as Pep508Requirement
-from tomlkit.exceptions import NonExistentKey
 
 from ._constraints import EditRequirement, Requirement, parse_requirement_from_pep508
 from ._pretty_specifier_set import PrettySpecifierSet
-
-_TomlDict: t.TypeAlias = (
-    tomlkit.container.Container
-    | tomlkit.container.OutOfOrderTableProxy
-    | tomlkit.items.AbstractTable
-)
+from ._toml import TomlRef, TomlRefRoot
 
 
 def edit_pyproject(pyproject: tomlkit.TOMLDocument, mapper: EditRequirement) -> None:
     """Apply the callback to each requirement specifier in the pyproject.toml file."""
-    _Editor(pyproject).apply(mapper)
+    _Editor.new(pyproject).apply(mapper)
 
 
+@t.final
 @dataclass
 class _Editor:
     # TODO skip path dependencies?
@@ -29,127 +24,64 @@ class _Editor:
     # TODO check if there are uv-specific fields
     # TODO support build dependencies?
 
-    pyproject: tomlkit.TOMLDocument
+    root: TomlRef
+
+    @classmethod
+    def new(cls, pyproject: tomlkit.TOMLDocument) -> t.Self:
+        return cls(TomlRefRoot(pyproject))
 
     def __post_init__(self) -> None:
-        self.project = _toml_get_table(self.pyproject, "project")
-        self.poetry = _toml_get_table(_toml_get_table(self.pyproject, "tool"), "poetry")
+        self.project = self.root["project"]
+        self.poetry = self.root["tool"]["poetry"]
 
     def apply(self, edit: EditRequirement) -> None:
         self._apply_all_pep621(edit)
         self._apply_all_poetry(edit)
 
     def _apply_all_pep621(self, edit: EditRequirement) -> None:
-        self._apply_requirements_array(
-            _toml_get_array(self.project, "dependencies"), edit
-        )
+        for ref in self.project["dependencies"].array_items():
+            self._apply_pep508_requirement(ref, edit)
 
-        optional_dependencies = _toml_get_table(self.project, "optional-dependencies")
-        for extra in optional_dependencies:
-            extra_dependencies = _toml_get_array(optional_dependencies, extra)
-            self._apply_requirements_array(extra_dependencies, edit)
+        for extra_ref in self.project["optional-dependencies"].table_entries():
+            for ref in extra_ref.array_items():
+                self._apply_pep508_requirement(ref, edit)
 
         # dependency groups, see <https://peps.python.org/pep-0735/>
-        dependency_groups = _toml_get_table(self.pyproject, "dependency-groups")
-        for group in dependency_groups:
-            group_dependencies = _toml_get_array(dependency_groups, group)
-            self._apply_requirements_array(group_dependencies, edit)
+        for group_ref in self.root["dependency-groups"].table_entries():
+            for ref in group_ref.array_items():
+                self._apply_pep508_requirement(ref, edit)
 
-    def _apply_requirements_array(
-        self, reqs: tomlkit.items.Array, edit: EditRequirement
-    ) -> None:
-        for i, item in enumerate(list(reqs)):
-            if not isinstance(item, str):
-                continue
-            req = Pep508Requirement(item)
-            data = parse_requirement_from_pep508(req)
-            edit.apply(data, kind="pep508")
-            new_specifier = PrettySpecifierSet(data["specifier"])
-            if req.specifier != new_specifier:
-                req.specifier = new_specifier
-                reqs[i] = str(req)
+    def _apply_pep508_requirement(self, ref: TomlRef, edit: EditRequirement) -> None:
+        raw_requirement = ref.value_as_str()
+        if raw_requirement is None:
+            return
+        req = Pep508Requirement(raw_requirement)
+        data = parse_requirement_from_pep508(req)
+        edit.apply(data, kind="pep508")
+        new_specifier = PrettySpecifierSet(data["specifier"])
+        if req.specifier != new_specifier:
+            req.specifier = new_specifier
+            ref.replace(str(req))
 
     def _apply_all_poetry(self, edit: EditRequirement) -> None:
         # cf https://python-poetry.org/docs/pyproject/#dependencies-and-dependency-groups
-        dependencies = _toml_get_table(self.poetry, "dependencies")
-        self._apply_poetry_dependency_table(dependencies, edit)
+        self._apply_poetry_dependency_table(self.poetry["dependencies"], edit)
 
-        groups = _toml_get_table(self.poetry, "group")
-        for group in groups:
-            group_dependencies = _toml_get_table(
-                _toml_get_table(groups, group), "dependencies"
-            )
-            self._apply_poetry_dependency_table(group_dependencies, edit)
+        for group_ref in self.poetry["group"].table_entries():
+            self._apply_poetry_dependency_table(group_ref["dependencies"], edit)
 
     def _apply_poetry_dependency_table(
-        self, reqs: _TomlDict, edit: EditRequirement
+        self, dependency_table_ref: TomlRef, edit: EditRequirement
     ) -> None:
-        name: str
-        for name in list(reqs.keys()):
-            target_table = reqs
-            target_key = name
-            value = reqs[name]
-            if isinstance(value, tomlkit.items.AbstractTable) and "version" in value:
-                target_table = value
-                target_key = "version"
-                value = value["version"]
-            if not isinstance(value, tomlkit.items.String):
+        for item_ref in dependency_table_ref.table_entries():
+            name = item_ref.key
+            version_ref: TomlRef = item_ref
+            if "version" in item_ref:
+                version_ref = item_ref["version"]
+            version = version_ref.value_as_str()
+            if version is None:
                 continue
-            req = Requirement(name=name, specifier=value.value)
+            req = Requirement(name=name, specifier=version)
             edit.apply(req, kind="poetry")
-            if value.value != req["specifier"]:
-                target_table[target_key] = req["specifier"]
-
-
-def _toml_get_table(container: _TomlDict, key: str) -> _TomlDict:
-    r"""Extract the table at that key, or return a null object table.
-
-    >>> doc = tomlkit.parse("a = 1\nb = {c = 3}")
-    >>> _toml_get_table(doc, "b")
-    {'c': 3}
-    >>> _toml_get_table(doc, "a")
-    {}
-    >>> _toml_get_table(doc, "nonexistent")
-    {}
-
-    This function can also extract interrupted tables, which involves a proxy type.
-    >>> doc = tomlkit.parse(
-    ...     '''\
-    ...     [table.a]
-    ...     content = 1
-    ...     [interrupted]
-    ...     [table.b]
-    ...     content = 2
-    ...     '''
-    ... )
-    >>> _toml_get_table(doc, "table")
-    {'a': {'content': 1}, 'b': {'content': 2}}
-
-    """
-    try:
-        value = container[key]
-    except NonExistentKey:
-        return tomlkit.table()
-    if not isinstance(value, _TomlDict):
-        return tomlkit.table()
-    return value
-
-
-def _toml_get_array(container: _TomlDict, key: str) -> tomlkit.items.Array:
-    r"""Extract the array at that key, or return a null object array.
-
-    >>> doc = tomlkit.parse("a = 1\nb = [42]")
-    >>> _toml_get_array(doc, "b")
-    [42]
-    >>> _toml_get_array(doc, "a")
-    []
-    >>> _toml_get_array(doc, "nonexistent")
-    []
-    """
-    try:
-        value = container[key]
-    except NonExistentKey:
-        return tomlkit.array()
-    if not isinstance(value, tomlkit.items.Array):
-        return tomlkit.array()
-    return value
+            if version != req["specifier"]:
+                version_ref.replace(req["specifier"])
